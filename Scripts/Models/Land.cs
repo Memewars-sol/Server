5bea21666bad8c383c7bea14b46eebafc21b540d
kida <leslug.away@gmail.com>
2024-03-15 16:58:40 +0800

    Added Book and Mint functions

--- Scripts/Models/Land.cs
+++ Scripts/Models/Land.cs
@@ -39,6 +39,24 @@ namespace Models {
             }
         }
 
+        public void Book(long id) {
+            if(IsBooked) {
+                throw new Exception("Land is already booked");
+            }
+
+            if(!string.IsNullOrEmpty(OwnerAddress)) {
+                throw new Exception("Land is already minted");
+            }
+            string query = string.Format("update lands set is_booked = true where id = {0}", id);
+            Database.ExecuteNonQuery(query);
+        }
+
+        public void Mint(string to_address) {
+            // function to middleware
+            string query = string.Format("update lands set owner_address = '{0}' where id = {1}", to_address, Id);
+            Database.ExecuteNonQuery(query);
+        }
+
         public static void AddCitizen(long land_id, long account_id) {
             string query = string.Format("insert into land_citizen (land_id, account_id) values ({0},{1})", land_id, account_id);
             Database.ExecuteNonQuery(query);
